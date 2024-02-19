dfd27183f0714099696ac8c8cd608bf7c960f87d
monke <porozheg@gmail.com>
2024-02-19 21:41:28 +0300

    add DateTime stamper and moved string generator to utils

--- src/client/utility/randomStringGenerator.h
+++ src/client/utility/randomStringGenerator.h
@@ -1,5 +1,5 @@
-#ifndef CLIENTSERVERQT_CLIENT_RANDOMSTRINGGENERATOR_RANDOMSTRINGGENERATOR_H_
-#define CLIENTSERVERQT_CLIENT_RANDOMSTRINGGENERATOR_RANDOMSTRINGGENERATOR_H_
+#ifndef CLIENTSERVERQT_CLIENT_UTILITY_RANDOMSTRINGGENERATOR_H_
+#define CLIENTSERVERQT_CLIENT_UTILITY_RANDOMSTRINGGENERATOR_H_
 
 #include <random>
 #include <QString>
@@ -13,16 +13,29 @@ namespace test{
             : _gen(std::random_device()()), _lengthDistribution(5, 50),
               _charNumDistribution(0, sizeof(charnumset) - 2), _charOnlyDistribution(9, sizeof(charnumset) - 2) {}
 
+        /**
+         * @brief generateNumCharString generates string consisting of random character and numbers with length 5-50
+         * @return generated string
+         */
         QString generateNumCharString() {
             return generateString(_charNumDistribution);
         }
-
+        /**
+         * @brief generateCharOnlyString generates string consisting of random character with length 5-50
+         * @return generated string
+         */
         QString generateCharOnlyString() {
             return generateString(_charOnlyDistribution);
         }
 
 
     private:
+        /**
+         * @brief generateString generates string based on charnumset and passed distribution
+         * @param dist - from to, _charNumDistribution to include both characters and numbers,
+         * _charOnlyDistribution for characters only
+         * @return generated string
+         */
         QString generateString(std::uniform_int_distribution<int> &dist){
             int length = dist(_gen);
             QString randString;
@@ -40,4 +53,4 @@ namespace test{
     };
 }
 
-#endif //CLIENTSERVERQT_CLIENT_RANDOMSTRINGGENERATOR_RANDOMSTRINGGENERATOR_H_
+#endif //CLIENTSERVERQT_CLIENT_UTILITY_RANDOMSTRINGGENERATOR_H_
